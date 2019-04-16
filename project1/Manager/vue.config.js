561e947cd66e7c6a70b719dee2cca92292cc1443
jasonandjay <342690199@qq.com>
2019-04-16 10:10:23 +0800

    获取到token，存储到cookie里

--- project1/Manager/vue.config.js
+++ project1/Manager/vue.config.js
@@ -31,17 +31,14 @@ module.exports = {
       errors: true
     },
     proxy: {
-      // change xxx-api/login => mock/login
-      // detail: https://cli.vuejs.org/config/#devserver-proxy
-      [process.env.VUE_APP_BASE_API]: {
-        target: `http://localhost:${port}/mock`,
+      '/api': {
+        target: `http://169.254.12.55:7001/`,
         changeOrigin: true,
         pathRewrite: {
-          ['^' + process.env.VUE_APP_BASE_API]: ''
+          '/api': ''
         }
       }
-    },
-    after: require('./mock/mock-server.js')
+    }
   },
   configureWebpack: {
     // provide the app's title in webpack's name field, so that
