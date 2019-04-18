ee3a245212f99af915a0c0ee99781b711dfabe6f
jasonandjay <342690199@qq.com>
2019-04-18 09:52:56 +0800

    增加导航守卫

--- project1/Manager/src/store/index.js
+++ project1/Manager/src/store/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import getters from './getters'
 
+import createLogger from 'vuex/dist/logger'
+
 Vue.use(Vuex)
 
 // https://webpack.js.org/guides/dependency-management/#requirecontext
@@ -19,7 +21,9 @@ const modules = modulesFiles.keys().reduce((modules, modulePath) => {
 
 const store = new Vuex.Store({
   modules,
-  getters
+  getters,
+
+  // plugins: [createLogger()]
 })
 
 export default store
--- project1/Manager/src/views/login/index.vue
+++ project1/Manager/src/views/login/index.vue
@@ -85,8 +85,8 @@ export default {
     }
     return {
       loginForm: {
-        username: 'chenmanjie',
-        password: 'Chenmanjie123!'
+        username: 'wuhongyang',
+        password: 'Wuhongyang123!'
       },
       loginRules: {
         username: [{ required: true, trigger: 'blur'}, {trigger:'blur', validator: validateUsername }],
@@ -122,8 +122,7 @@ export default {
   },
   methods: {
     ...mapActions({
-      login: 'user/login',
-      generateRoutes: 'permission/generateRoutes'
+      login: 'user/login'
     }),
     showPwd() {
       if (this.passwordType === 'password') {
@@ -143,7 +142,6 @@ export default {
           let res = await this.login(this.loginForm);
           console.log('login res...', res);
           if (res.code == 1){
-            await this.generateRoutes([]);
             this.$router.push({ path: this.redirect || '/' })
           }
           this.loading = false
